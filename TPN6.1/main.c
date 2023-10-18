ffde314df22d458d5ff2144919a8af9f9fb84212
Axel E. de la Canal <axel.delacanal.aedlc@gmail.com>
2023-10-18 15:17:36 -0300

    Casi Finalizado

--- TPN6.1/main.c
+++ TPN6.1/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -35,9 +35,9 @@ typedef struct
 nodo* crearNodo(int nota, int legajo, char nombreApe[]);
 int buscarPosMateria(celda arreglo[], char materia[], int validos);
 int agregarMateria(celda arreglo[], char materia[], int validos);
-nodo* agregaAlPpio(nodo* lista, nodo* nuevoNodo);
-int alta(celda arreglo[], char materia[], int nota, int legajo, char nombreApe[], int validos);
-int ingresarnotas(celda arreglo[], int dimension);
+nodo* agregarAlPrincipio(nodo* lista, nodo* nuevoNodo);
+int cargarEnArreglo(celda arreglo[], char materia[], int nota, int legajo, char nombreApe[], int validos);
+int ingresarNotas(celda arreglo[], int dimension, int validos);
 void mostrarNotasPorMateria(celda arreglo[], int validos, char materia[]);
 
 
@@ -46,7 +46,7 @@ int main()
 {
     celda arreglo[20];
 
-    int validos = ingresarnotas(arreglo, 20);
+    int validos = ingresarNotas(arreglo, 20, validos);
 
     char materia[40];
     printf("\nIngrese la materia para mostrar las notas: ");
@@ -57,6 +57,187 @@ int main()
     return 0;
 }
 
+
+// Función para ingresar notas y detalles de estudiantes
+int ingresarNotas(celda arreglo[], int dimension, int validos)
+{
+    int nota;
+    int legajo;
+    char nombreApe[40];
+    char materia[40];
+    char salir = 's';
+
+    while (salir == 's' && validos < dimension)
+    {
+        printf("Ingrese la materia: ");
+        fflush(stdin);
+        gets(&materia);
+
+        printf("\nIngrese un nota: ");
+        fflush(stdin);
+        scanf("%d", &nota);
+
+        printf("\nIngrese un legajo: ");
+        fflush(stdin);
+        scanf("%d", &legajo);
+
+        printf("\nIngrese un nombre y apellido: ");
+        fflush(stdin);
+        gets(&nombreApe);
+
+        validos = cargarEnArreglo(arreglo, materia, nota, legajo, nombreApe, validos);  // Se da de alta al estudiante en la materia
+
+        printf("\nDesea continuar s/n \n");
+        fflush(stdin);
+        scanf("%c", &salir);
+    }
+    return validos;
+}
+
+
+
+*/
+///---------------------------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    int nota;
+    int legajo;
+    char nombreApe[40];
+    char materia[40];
+    int idMateria;
+} registroArchivo;
+
+typedef struct
+{
+    int nota;
+    int legajo;
+    char nombreApe[40];
+} notaAlumno;
+
+typedef struct _nodo
+{
+    notaAlumno dato;
+    struct _nodo* siguiente;
+} nodo;
+
+typedef struct
+{
+    int idMateria;
+    char materia[40];
+    nodo* listaDeNotas;
+} celda;
+
+///PROTOTIPADOS:
+registroArchivo cargarRegistro();
+void cargarArchivo();
+void mostrarRegistro(registroArchivo nuevoRegistro);
+void mostrarArchivo();
+
+
+
+int main()
+{
+    celda arreglo[100];  // Tamaño máximo del arreglo
+    int validos = 0;
+
+    //cargarArchivo();
+    pasarDeArchivoToADL(arreglo, &validos);
+    mostrarArchivoArreglo(arreglo, validos);
+
+    return 0;
+}
+
+
+
+registroArchivo cargarRegistro()
+{
+    registroArchivo nuevoRegistro;
+
+    printf("Ingrese la Nota: ");
+    fflush(stdin);
+    scanf("%i", &nuevoRegistro.nota);
+
+    printf("\nIngrese el legajo: ");
+    fflush(stdin);
+    scanf("%i", &nuevoRegistro.legajo);
+
+    printf("\nIngrese Nombre y Apellido: ");
+    fflush(stdin);
+    gets(&nuevoRegistro.nombreApe);
+
+    printf("\nIngrese la materia: ");
+    fflush(stdin);
+    gets(&nuevoRegistro.materia);
+
+    printf("\nIngrese id materia: ");
+    fflush(stdin);
+    scanf("%i", &nuevoRegistro.idMateria);
+
+    return nuevoRegistro;
+}
+
+void cargarArchivo()
+{
+    char seguir = 's';
+    registroArchivo nuevoRegistro;
+
+    FILE* ptr = fopen("archivoRegistro.txt", "a");
+
+    if(ptr != NULL)
+    {
+        printf("REGISTROS: \n");
+
+        while(seguir == 's')
+        {
+            nuevoRegistro = cargarRegistro();
+            fwrite(&nuevoRegistro, sizeof(registroArchivo),1, ptr);
+
+            printf("Quiere cargar otro registro? s/n\n");
+            fflush(stdin);
+            scanf("%c", &seguir);
+        }
+        fclose(ptr);
+    }
+    else
+    {
+        printf("ERROR: No se pudo abrir el archivo.");
+    }
+}
+
+void mostrarRegistro(registroArchivo nuevoRegistro)
+{
+    printf("\n-------------------------------------------\n");
+    printf("NOTA: %i\n", nuevoRegistro.nota);
+    printf("LEGAJO: %i\n", nuevoRegistro.legajo);
+    printf("NOMBRE Y APELLIDO: %s\n", nuevoRegistro.nombreApe);
+    printf("MATERIO: %s\n", nuevoRegistro.materia);
+    printf("ID MATERIA: %i\n", nuevoRegistro.idMateria);
+    printf("-------------------------------------------\n");
+
+}
+
+void mostrarArchivo()
+{
+    registroArchivo nuevoRegistro;
+    FILE* ptr = fopen("archivoAprobados.txt", "r");
+
+    if(ptr != NULL)
+    {
+        while(fread(&nuevoRegistro, sizeof(registroArchivo),1, ptr) > 0)
+        {
+            mostrarRegistro(nuevoRegistro);
+        }
+        fclose(ptr);
+    }
+    else
+    {
+        printf("ERROR: No se pudo abrir el archivo.");
+    }
+}
 
 // Función para crear un nuevo nodo
 nodo* crearNodo(int nota, int legajo, char nombreApe[])
@@ -95,7 +276,7 @@ int agregarMateria(celda arreglo[], char materia[], int validos)
 }
 
 // Función para agregar un nodo al principio de una lista enlazada
-nodo* agregaAlPpio(nodo* lista, nodo* nuevoNodo)
+nodo* agregarAlPrincipio(nodo* lista, nodo* nuevoNodo)
 {
     if (lista == NULL)
     {
@@ -110,7 +291,7 @@ nodo* agregaAlPpio(nodo* lista, nodo* nuevoNodo)
 }
 
 // Función para dar de alta un estudiante en una materia
-int alta(celda arreglo[], char materia[], int nota, int legajo, char nombreApe[], int validos)
+int cargarEnArreglo(celda arreglo[], char materia[], int nota, int legajo, char nombreApe[], int validos)
 {
     nodo* aux = crearNodo(nota, legajo, nombreApe);  // Se crea un nuevo nodo con los datos del estudiante
     int pos = buscarPosMateria(arreglo, materia, validos);  // Se busca la posición de la materia en el arreglo
@@ -120,44 +301,8 @@ int alta(celda arreglo[], char materia[], int nota, int legajo, char nombreApe[]
         validos = agregarMateria(arreglo, materia, validos);  // Si la materia no existe, se agrega al arreglo
         pos = validos - 1;  // La posición es la última del arreglo
     }
-    arreglo[pos].listaDeNotas = agregaAlPpio(arreglo[pos].listaDeNotas, aux);  // Se agrega el nodo a la lista de notas de la materia
+    arreglo[pos].listaDeNotas = agregarAlPrincipio(arreglo[pos].listaDeNotas, aux);  // Se agrega el nodo a la lista de notas de la materia
 
-    return validos;
-}
-
-// Función para ingresar notas y detalles de estudiantes
-int ingresarnotas(celda arreglo[], int dimension)
-{
-    int nota;
-    int legajo;
-    char nombreApe[40];
-    char materia[40];
-    char salir = 's';
-    int validos = 0;
-
-    while (salir == 's' && validos < dimension)
-    {
-        printf("\ningrese la materia: ");
-        fflush(stdin);
-        gets(&materia);
-
-        printf("\ningrese un nota: ");
-        fflush(stdin);
-        scanf("%d", &nota);
-
-        printf("\ningrese un legajo: ");
-        fflush(stdin);
-        scanf("%d", &legajo);
-
-        printf("\ningrese un nombre y apellido: ");
-        fflush(stdin);
-        gets(&nombreApe);
-
-        validos = alta(arreglo, materia, nota, legajo, nombreApe, validos);  // Se da de alta al estudiante en la materia
-
-        printf("Desea continuar presione 's'");
-        salir = getchar();  // Se obtiene la opción del usuario para continuar
-    }
     return validos;
 }
 
@@ -183,3 +328,56 @@ void mostrarNotasPorMateria(celda arreglo[], int validos, char materia[])
         printf("No se encontro la materia especificada.\n");
     }
 }
+
+
+///… pasarDeArchivoToADL(…) ---> esta función recorrerá el archivo y pasará la información al arreglo de listas
+///(utilizando las funciones necesarias que usted ya codificó).
+void pasarDeArchivoToADL(celda arreglo[], int *validos)
+{
+    FILE *ptr = fopen("archivoRegistro.txt", "r");
+
+    if (ptr != NULL)
+    {
+        registroArchivo nuevoRegistro;
+        while (fread(&nuevoRegistro, sizeof(registroArchivo), 1, ptr) > 0)
+        {
+            *validos = cargarEnArreglo(arreglo, nuevoRegistro.materia, nuevoRegistro.nota, nuevoRegistro.legajo, nuevoRegistro.nombreApe, *validos);
+        }
+        fclose(ptr);
+    }
+    else
+    {
+        printf("ERROR: No se pudo abrir el archivo.");
+    }
+}
+
+
+void mostrarArchivoArreglo(celda arreglo[], int validos)
+{
+    printf("----------------------------MATERIAS Y NOTAS:----------------------------\n");
+
+    for (int i = 0; i < validos; i++)
+    {
+        printf("MATERIA: %s\n", arreglo[i].materia);
+        printf("ID Materia: %d\n", arreglo[i].idMateria);
+
+        nodo *actual = arreglo[i].listaDeNotas;
+
+        if (actual != NULL)
+        {
+            while (actual != NULL)
+            {
+                printf("Legajo: %d, Nombre y Apellido: %s, NOTAS: %d\n", actual->dato.legajo, actual->dato.nombreApe, actual->dato.nota);
+                actual = actual->siguiente;
+            }
+        }
+        else
+        {
+            printf("No hay notas registradas para esta materia.\n");
+        }
+        printf("-------------------------------------------\n");
+    }
+}
+
+
+
