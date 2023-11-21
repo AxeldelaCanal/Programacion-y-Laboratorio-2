9aea322a4006b9d306621e8df510ced09542e065
Axel E. de la Canal <axel.delacanal.aedlc@gmail.com>
2023-11-21 11:39:51 -0300

    Initial commit

---
+++ ProyectoFinal/Menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+void opcion1();
+void opcion2();
+int anchoConsola;
+void centrarTextoAuto(const char *texto, int y);
+
+#endif // MENU_H_INCLUDED
---
+++ ProyectoFinal/main.c
@@ -0,0 +1,52 @@
+#include "Empleado.h"
+#include "Paciente.h"
+#include "Menu.h"
+#include <windows.h>
+
+int main()
+{
+    Paciente *raiz = NULL;
+    cargarDatosDesdeArchivoMejorado(&raiz);
+    int dniMostrar, dniAEliminar, dniConsulta, dniABuscar, opcion, eleccion = 0, fila = 5;
+    char entrada[100], fechaDesde[11], fechaHasta[11], usuario[20], contrasenia[20];
+
+    do
+    {
+        centrarTextoAuto("Bienvenido. Ya estas registrado?\n", fila++);
+        centrarTextoAuto("1. Si\n", fila++);
+        centrarTextoAuto("2. No\n", fila++);
+        centrarTextoAuto("Seleccione una opcion: \n", fila);
+
+        COORD posicionCursor;
+        posicionCursor.X = (anchoConsola - 1) / 2;
+        posicionCursor.Y = fila;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), posicionCursor);
+
+        fgets(entrada, sizeof(entrada), stdin); // Leer entrada como texto
+        int entradaValida = sscanf(entrada, "%d", &eleccion);
+
+
+        // Validar la entrada
+        if (entradaValida != 1 || (eleccion != 1 && eleccion != 2))
+        {
+            centrarTextoAuto("Error. Vuelva a intentarlo: \n", fila);
+            system("pause");
+            system("cls");
+        }
+
+        if (eleccion == 1)
+        {
+            opcion1();
+        }
+        else if (eleccion == 2)
+        {
+            opcion2();
+            printf("Por favor a continuacion ingrese con su usuario. \n");
+            system("pause");
+            opcion1();
+        }
+    }
+    while(eleccion != 1 && eleccion != 2);
+    guardarDatosEnArchivo(raiz);
+    return 0;
+}
